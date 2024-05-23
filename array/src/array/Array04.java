bb89f09f92d32b7fa15858e0c095bcce9b4a92cd
김형진 <hyoung6658@gmail.com>
2024-05-23 11:28:46 +0900

    입력한 9개의 정수배열의 최댓값과 위치를 찾는 코드

--- array/src/array/Array04.java
+++ array/src/array/Array04.java
@@ -7,27 +7,24 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Array04 {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		int[] arr = new int[9];
-		int index = 0;
-		int max = 0;
-		
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		
-		for(int i = 0;i < 9;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			
-			
-			if(max < arr[i]) {
-				max = arr[i];
-				index = (i+1);
-			}
-			
-		}
-		System.out.println(max + "\n" + index);
-	}
+    public static void main(String[] args) throws IOException {
 
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        int index = 0;
+        int max = 0;
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+			
+            if (max < arr[i]) {
+                max = arr[i];
+                index = (i + 1);
+            }
+        }
+        System.out.println(max + "\n" + index);
+    }
 }
