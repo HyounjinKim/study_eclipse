a04f51c858a107fd8efb0ab3440ad55a4536e874
김형진 <hyoung6658@gmail.com>
2024-05-23 11:25:36 +0900

    배열의 최솟값과 최댓값을 찾아서 출력하는 코드

--- array/src/array/Array03.java
+++ array/src/array/Array03.java
@@ -7,22 +7,20 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Array03 {
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(bf.readLine());
-		int[] arr = new int[N];
-		
-		StringTokenizer st = new StringTokenizer(bf.readLine());
-		
-		for(int i = 0;i < N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(arr);
-		
-		System.out.println(arr[0] + " " + arr[N-1]);
-	}
+    public static void main(String[] args) throws IOException {
 
-		
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bf.readLine());
+        int[] arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] + " " + arr[N - 1]);
+    }
 }
