bca87531a53c41d9b514d79b77b7400a437d2ca6
김형진 <hyoung6658@gmail.com>
2024-05-25 15:45:55 +0900

    2차원 배열 랜덤값 출력

--- ex10/src/ex10/Ex01.java
+++ ex10/src/ex10/Ex01.java
@@ -1,32 +1,30 @@
 package ex10;
 
 public class Ex01 {
-	public static void main(String[] args) {
-		
-		int[][] arr = new int[5][5];
-		int[][] brr = new int[5][5];
-		
-		doPointArr(arr);
-		System.out.println(3);
-		doPointArr(brr);
-	}
+    public static void main(String[] args) {
 
-	public static void doPointArr(int[][] arr) {
-		
+        int[][] arr = new int[5][5];
+        int[][] brr = new int[5][5];
 
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
+        doPointArr(arr);
+        System.out.println(3);
+        doPointArr(brr);
+    }
 
-				arr[i][j] = (int) (Math.random() * 100) + 1;
-			}
+    public static void doPointArr(int[][] arr) {
 
-			for (int j = 0; j < arr.length; j++) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
 
-				arr[i][j] = (int) (Math.random() * 100) + 1;
-				System.out.print(arr[i][j] + "\t");
-			}
-			System.out.println("");
-		}
-	}
+                arr[i][j] = (int) (Math.random() * 100) + 1;
+            }
 
+            for (int j = 0; j < arr.length; j++) {
+
+                arr[i][j] = (int) (Math.random() * 100) + 1;
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+    }
 }
--- ex10/src/ex10/Ex03.java
+++
@@ -1,7 +0,0 @@
-package ex10;
-
-public class Ex03 {
-	public static void main(String[] args) {
-
-	}
-}
