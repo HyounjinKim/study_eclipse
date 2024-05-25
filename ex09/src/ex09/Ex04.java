31eb369e46c83899c52886dd83e0b5644a127855
김형진 <hyoung6658@gmail.com>
2024-05-25 15:29:48 +0900

    버블정렬

--- ex09/src/ex09/Ex04.java
+++ ex09/src/ex09/Ex04.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 /*
  * 버블정렬
- * 
+ *
  * 1회전
  * {100,90,60,40,120}
  * {90,100,60,40,120}
@@ -15,32 +15,30 @@ import java.util.Arrays;
  * {60,40,90,100,120}
  * 3회전
  * {40,60,90,100,120}
- * 
+ *
  * {100,90,60,40,120}
- * 
+ *
  * 최대값
  * 최소값
- * 
+ *
  * 내림차순
  * 오름차순
  */
 
 public class Ex04 {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int[] arr = { 100, 90, 60, 40, 120 };
+        int[] arr = {100, 90, 60, 40, 120};
 
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5 - j; j++) {
-				if (arr[j] > arr[j + 1]) {
-					int temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
-				}
-
-			}
-		}
-		System.out.println(Arrays.toString(arr));
-	}
-
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5 - j; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
