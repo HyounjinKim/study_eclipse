4c442e5d1e03c12c5dab7eef365273e70b8b342e
김형진 <hyoung6658@gmail.com>
2024-05-25 15:47:36 +0900

    Tv 클래스의 변수 / 함수 호출

--- ex10/src/ex10/Ex02.java
+++ ex10/src/ex10/Ex02.java
@@ -2,8 +2,7 @@ package ex10;
 
 public class Ex02 {
 	public static void main(String[] args) {
-
-		int a = 10;
+		
 		Tv tv1 = new Tv();
 
 		System.out.println(tv1.color);
@@ -13,11 +12,10 @@ public class Ex02 {
 		tv1.power();
 		
 		System.out.println(tv1.power);
-		tv1.channelDown();;
+		tv1.channelDown();
 		System.out.println(tv1.channel);
 		tv1.channelUp();
 		System.out.println(tv1.channel);
 
-//		tv = null;
 	}
 }
