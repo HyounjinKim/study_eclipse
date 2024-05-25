a1f52d6a5dbb51257fbf76bb3fa8306105f84997
김형진 <hyoung6658@gmail.com>
2024-05-25 16:25:59 +0900

    문자열을 아스키코드로 변환해서 int배열에 넣고 계산하는 코드

--- ex10/src/ex10/Solution.java
+++ ex10/src/ex10/Solution.java
@@ -1,33 +1,31 @@
 package ex10;
 
 public class Solution {
-	public Solution() {
-		String num = "123";
+    public Solution() {
+        String num = "123";
 
-		solution(num);
-	}
+        solution(num);
+    }
 
-	public int solution(String number) {
-		int answer = 0;
-		char[] arr = number.toCharArray();
-		int[] brr = new int[arr.length];
-		int sum = 0;
+    public int solution(String number) {
+        int answer = 0;
+        char[] arr = number.toCharArray();
+        int[] brr = new int[arr.length];
+        int sum = 0;
 
-		for (int i = 0; i < arr.length; i++) {
-			brr[i] = arr[i] - 48;
-		}
-		for (int i = 0; i < arr.length; i++) {
-			sum = sum + brr[i];
-		}
+        for (int i = 0; i < arr.length; i++) {
+            brr[i] = arr[i] - 48;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + brr[i];
+        }
 
-		answer = sum % 9;
-		System.out.println(answer);
-		return answer;
-	}
+        answer = sum % 9;
+        System.out.println(answer);
+        return answer;
+    }
 
-	public static void main(String[] args) {
-		new Solution(); // heap 영역
-
-	}
-
+    public static void main(String[] args) {
+        new Solution(); // heap 영역
+    }
 }
