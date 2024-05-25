d43ab22d9189ffbcedba8567b5ca46f3cb1a2568
김형진 <hyoung6658@gmail.com>
2024-05-25 15:11:00 +0900

    배열 카피

--- ex08/src/ex08/Ex06.java
+++ ex08/src/ex08/Ex06.java
@@ -7,16 +7,11 @@ public class Ex06 {
 
 		int[] arr = {1,2,3,4,5,6,7,8,9};
 		int[] brr = Arrays.copyOf(arr, 10);
-//		int[] brr = new int[arr.length];
-//		
-//		for(int i = 0; i < arr.length;i++) {
-//			brr[i] = arr[i];
-//		}
+
 		arr[0] = 100;
 		
 		System.out.println(Arrays.toString(arr));
 		System.out.println(Arrays.toString(brr));
 		
 	}
-
 }
