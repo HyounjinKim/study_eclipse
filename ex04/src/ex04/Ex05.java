28d4c540ddfec4a115d1470bf15e058f0ffb288e
김형진 <hyoung6658@gmail.com>
2024-05-25 13:52:09 +0900

    문자열 덮어쓰기

---
+++ ex04/src/ex04/Ex05.java
@@ -0,0 +1,23 @@
+package ex04;
+
+public class Ex07 {
+    public static void main(String[] args) {
+
+        String str1 = "He22oWor2d";
+        String str2 = "lloWorl";
+        int s = 2;
+        char[] cs1;
+        char[] cs2;
+
+        cs1 = str1.toCharArray(); //10
+        cs2 = str2.toCharArray(); //7
+
+        for (int i = 0; i < cs2.length + 2; i++) {
+            if (i >= s) {
+                cs1[i] = cs2[i - s];
+            }
+        }
+        String str = new String(cs1);
+        System.out.println(str);
+    }
+}
