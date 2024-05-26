c59d1bd0071cee29a7089cbaad79c683a17fff72
김형진 <hyoung6658@gmail.com>
2024-05-26 14:38:37 +0900

    정적 변수를 참조하여 정적 메서드를 호출하는 예시

--- ex13/src/ex13/Ex03.java
+++ ex13/src/ex13/Ex03.java
@@ -1,28 +1,27 @@
 package ex13;
 
-class AA{
-	public static void doPrint() {
-		System.out.println("스태틱 doPrint");
-	}
+class AA {
+    public static void doPrint() {
+        System.out.println("스태틱 doPrint");
+    }
 }
 
 class AAA {
-	public static AA aa= new AA();
-	
-	public void doA() {
-		System.out.println("AAA doA");
-	}
+    public static AA aa = new AA();
+
+    public void doA() {
+        System.out.println("AAA doA");
+    }
 }
 
 public class Ex03 {
-	public static void main(String[] args) {
-		AAA a = new AAA();
-		a.doA();
-		
-		AAA.aa.doPrint();
-		
-		System.out.println("테스트");
+    public static void main(String[] args) {
+        AAA a = new AAA();
+        a.doA();
 
-	}
+        AAA.aa.doPrint();
 
+        System.out.println("테스트");
+
+    }
 }
