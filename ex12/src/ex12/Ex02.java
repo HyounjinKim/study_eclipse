2ef6512fb522612097237363b6ffc81636d2729e
김형진 <hyoung6658@gmail.com>
2024-05-26 14:12:56 +0900

    다른 패키지에 동일한 클래스명이 있을때 참조법

--- ex12/src/ex12/Ex02.java
+++ ex12/src/ex12/Ex02.java
@@ -1,10 +1,10 @@
 package ex12;
 
 public class Ex02 {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		aaa.AA a1 = new aaa.AA();
-		ex12.AA a2 = new ex12.AA();
+        aaa.AA a1 = new aaa.AA();
+        ex12.AA a2 = new ex12.AA();
 
-	}
+    }
 }
