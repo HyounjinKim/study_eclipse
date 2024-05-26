6e91235b345c56c349c588488f837e223695e690
김형진 <hyoung6658@gmail.com>
2024-05-26 14:31:19 +0900

    인스턴스 생성시 생성자 호출 예제

--- ex13/src/ex13/Ex02.java
+++ ex13/src/ex13/Ex02.java
@@ -1,13 +1,13 @@
 package ex13;
 
 public class Ex02 {
-	public static void main(String[] args) {
-		InstClass ic = new InstClass();
-		
-		System.out.println(ic.num++);
+    public static void main(String[] args) {
+        InstClass ic = new InstClass();
 
-		System.err.println(InstClass.num++);
+        System.out.println(ic.num++);
 
-		System.err.println(InstClass.num);
-	}
+        System.err.println(InstClass.num++);
+
+        System.err.println(InstClass.num);
+    }
 }
