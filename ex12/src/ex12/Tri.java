a103902f04a741c18cf35585608f3cd5573c4533
김형진 <hyoung6658@gmail.com>
2024-05-26 14:21:55 +0900

    삼각형 넓이 계산 클래스

--- ex12/src/ex12/Tri.java
+++ ex12/src/ex12/Tri.java
@@ -4,15 +4,15 @@ public class Tri {
 	int w;
 	int h;
 
-	public Tri() {	};
+	public Tri() {};
 
 	public Tri(int w, int h) {
 		this.w = w;
 		this.h = h;
 	}
-	
+
 	public double area() {
-		
+
 		return w * h * 0.5;
 	}
 
