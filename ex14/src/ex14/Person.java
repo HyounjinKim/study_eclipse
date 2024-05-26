09ef4cd7e562a783666eea883e97f5fcb4beb2c7
김형진 <hyoung6658@gmail.com>
2024-05-26 14:52:20 +0900

    오버로딩 객체 초기화 방법

--- ex14/src/ex14/Person.java
+++ ex14/src/ex14/Person.java
@@ -2,25 +2,24 @@ package ex14;
 
 public class Person {
 
-	private int regNum;
-	private int passNum;
+    private int regNum;
+    private int passNum;
 
-	public Person(int regNum) {
-		System.out.println("regNum 생성자");
-		this.regNum = regNum;
-	}
+    public Person(int regNum) {
+        System.out.println("regNum 생성자");
+        this.regNum = regNum;
+    }
 
-	public Person(int regNum, int passNum) {
-		this(regNum);
-		System.out.println("regNum 생성자 passNum 생성자");
-		
+    public Person(int regNum, int passNum) {
+        this(regNum);
+        System.out.println("regNum 생성자 passNum 생성자");
+
 //		this.regNum = regNum;
-		this.passNum = passNum;
-	}
+        this.passNum = passNum;
+    }
 
-	@Override
-	public String toString() {
-		return "Person [regNum=" + regNum + ", passNum=" + passNum + "]";
-	}
-
+    @Override
+    public String toString() {
+        return "Person [regNum=" + regNum + ", passNum=" + passNum + "]";
+    }
 }
