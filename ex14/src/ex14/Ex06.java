8ed12b95392ccb681f96b9759b17801c2d3fe77a
김형진 <hyoung6658@gmail.com>
2024-05-26 14:59:14 +0900

    String.valueOf

--- ex14/src/ex14/Ex06.java
+++ ex14/src/ex14/Ex06.java
@@ -1,18 +1,17 @@
 package ex14;
 
 public class Ex06 {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		int a = 10;
-		double b = 20.5;
-		String c = String.valueOf(a);
-		String d = String.valueOf(b);
+        int a = 10;
+        double b = 20.5;
+        String c = String.valueOf(a);
+        String d = String.valueOf(b);
 
-		String e = a+"";
-		
-		System.out.println(c);
-		System.out.println(d);
-		System.out.println(e);
-	}
+        String e = a + "";
 
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
 }
